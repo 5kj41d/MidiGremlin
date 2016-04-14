8997f83d917ad354344ff20015cce7eff1de72eb
Johannes Elgaard <joha4270@gmail.com>
2016-04-14 09:43:54 +0200

    Initial commit

---
+++ src/MidiGremlin/Orchestra.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidiGremlin
+{
+    class Orchestra
+    {
+        //TODO:List of instruments
+        public Orchestra (IMidiOut output)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Instrument AddInstrument ()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
