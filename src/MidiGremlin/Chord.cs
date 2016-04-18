413052bd73b4973ca2d47ea656bc8eff847d8184
Fynfner <kennetnlarsen@gmail.com>
2016-04-18 13:04:09 +0200

    Implemented Chord and ChordInstance.

--- src/MidiGremlin/Chord.cs
+++ src/MidiGremlin/Chord.cs
@@ -11,9 +11,11 @@ namespace MidiGremlin
     ///</summary>
     public class Chord
     {
+        int[] _toneSteps;
+        const int _defaultDuration = 2;
         public Chord(params int[] toneSteps)
         {
-                throw new NotImplementedException();
+            _toneSteps = toneSteps;
         }
 
         public static Chord Name(string name)
@@ -23,10 +25,28 @@ namespace MidiGremlin
             
         //TODO Og så videre. Muligvis ikke rent faktisk en dur det her.
         public static Chord Major { get; } = new Chord(4, 7);
-
-        public MusicObject WithBaseTone(Tone tone)
+        /// <summary>
+        /// Creates a new instance of the chord with a default value duration and velocity using a tone as the value of the root.
+        /// </summary>
+        /// <param name="tone">A Tone enum value depicting the root of the chord.</param>
+        /// <returns></returns>
+        public MusicObject this[Tone tone]
         {
-            return new ChordInstance();
+            get
+            {
+                return new ChordInstance(tone, _defaultDuration, _toneSteps);
+            }
+        }
+        /// <summary>
+        /// Creates a new instance of the chord with a given duration and velocity, using a tone as the value of the root.
+        /// </summary>
+        /// <param name="tone">A Tone enum value depicting the root of the chord.</param>
+        /// <param name="duration">The duration of the chord.</param>
+        /// <param name="velocity">The velocity of the chord.</param>
+        /// <returns></returns>
+        public MusicObject WithBaseTone(Tone tone, int duration, byte velocity)
+        {
+            return new ChordInstance(tone,duration, velocity, _toneSteps);
         }
     }
 }
