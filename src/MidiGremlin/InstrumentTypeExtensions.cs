10b170765620139e94f6017a4a2549994479c704
Johannes Elgaard <joha4270@gmail.com>
2016-05-03 13:38:16 +0200

    Bugfixes, sorry did too much, forgot all

---
+++ src/MidiGremlin/InstrumentTypeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace MidiGremlin
+{
+	public static class InstrumentTypeExtensions
+	{
+		public static bool IsDrum(this InstrumentType instrumentType)
+		{
+			return ((int)instrumentType & 0x7fffff80) == 0x100;
+		}
+	}
+}
