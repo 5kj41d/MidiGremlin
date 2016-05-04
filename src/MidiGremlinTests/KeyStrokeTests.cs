f8d52356bc4cf7fbaa46b8ca8895493124697261
RegsaGC <asgerhb@gmail.com>
2016-05-04 15:01:56 +0200

    Bedre Keystroke.OffsetBy med alle relevante tests denne gang

--- src/MidiGremlinTests/KeyStrokeTests.cs
+++ src/MidiGremlinTests/KeyStrokeTests.cs
@@ -45,5 +45,21 @@ namespace MidiGremlin.Tests
             Keystroke result = k.OffsetBy(offset, octaveOffset);            
             Assert.AreEqual(expected.Tone, result.Tone);
         }
+
+        [TestCase(Tone.B, 1, 0, Tone.C + 12)]
+        [TestCase(Tone.D, 1, 0, Tone.B)]
+        [TestCase(Tone.B, 0, 1, Tone.B + 12)]
+        [TestCase(Tone.C, 4, 0, Tone.D)]
+        [Test]
+        public void OffsetInScaleTest (Tone startTone, int offset, int octaveOffset, Tone expectedTone)
+        {
+            Scale scale = new Scale(Tone.C, Tone.D, Tone.B);
+            Keystroke k = new Keystroke(startTone, 1);
+
+            Keystroke expected = new Keystroke(expectedTone, 1);
+            Keystroke result = k.OffsetBy(scale, offset, octaveOffset);
+
+            Assert.AreEqual(expected.Tone, result.Tone);
+        }
     }
 }
