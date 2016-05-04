37f12ce6e3d23942637de1e641628da1520608f7
RegsaGC <asgerhb@gmail.com>
2016-05-04 14:54:23 +0200

    Bedre Keystroke.OffsetBy plus unit tests

--- src/MidiGremlinTests/NoteTests.cs
+++ src/MidiGremlinTests/NoteTests.cs
@@ -28,5 +28,23 @@ namespace MidiGremlin.Tests
             SingleBeat expected = new SingleBeat(i.InstrumentType, expectedPitch, n.Velocity, 0, n.Duration);
             Assert.AreEqual(actual, expected);
         }
+
+
+        [TestCase(Tone.B, 1, 0, Tone.C + 12)]
+        [TestCase(Tone.C, 0, 1, Tone.C + 12)]
+        [TestCase(Tone.B, 1, 1, Tone.C + 12*2)]
+        [TestCase(Tone.B, 0, 99, Tone.B + 12*99)]
+        [TestCase(Tone.C, 12, 0, Tone.C + 12)]
+        [TestCase(Tone.C, 14, 0, Tone.D + 12)]
+        [Test]
+        public void OffsetTest(Tone startTone, int offset, int octaveOffset, Tone expectedTone)
+        {
+            Keystroke k = new Keystroke(startTone, 1);
+
+            Keystroke expected = new Keystroke(expectedTone, 1);
+            Keystroke result = k.OffsetBy(offset, octaveOffset);
+
+            Assert.AreEqual(expected.Tone, result.Tone);
+        }
     }
 }
