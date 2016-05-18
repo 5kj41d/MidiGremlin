343d72966ecc8ea4be6462869390878128c28118
dark223 <nick.sneftrup@hotmail.com>
2016-05-18 13:38:45 +0200

    minimal exampel

---
+++ eksamples/MinimalExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MidiGremlin;
+
+
+namespace MinimalExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Orchestra orchestra = new Orchestra(new WinmmOut(0, 70));
+            Instrument bagpipe = orchestra.AddInstrument(InstrumentType.BagPipe);
+            bagpipe.Play(Tone.CSharp, 1);
+            orchestra.WaitForFinished();
+        }
+    }
+}
